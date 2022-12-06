cbefd7b2990b7396fd62d04ac9690cd99b9d7936
MKamui <marioelkamui@gmail.com>
2022-12-06 16:31:03 -0500

    feat(boiler completo)

---
+++ src/redux/reducers/reducerApp.js
@@ -0,0 +1,13 @@
+const initialState = {
+  
+};
+
+export function reducerApp(state = initialState, action) {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+}
+
+export default reducerApp;
