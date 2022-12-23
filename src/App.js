2d1ba164e411e125295517967c034ae18d1b0e64
MKamui <marioelkamui@gmail.com>
2022-12-22 19:17:04 -0500

    feat(Landing/NavBar)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Landing from "./components/Landing";
 import Home from "./components/Home";
 import Country from "./components/Country";
 import { Routes, Route } from "react-router-dom";
+import Forum from "./components/Forum";
+import Account from "./components/Account";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">HenryHouse</h1>
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route exact path="/home" element={<Home />} />
+        <Route exact path="/foro" element={<Forum />} />
+        <Route exact path="/cuenta" element={<Account />} />
         <Route exact path="/foro/:pais" element={<Country />} />
+        <Route exact path="/*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return(
+    <div className="bg-black absolute w-[100%] text-yellow-300 flex z-10 items-center">
+      <img src="https://assets.soyhenry.com/LOGO-REDES-01_og.jpg" alt="logo" className="w-14 h-10"/>
+      <Link to="/home"><h1 className="ml-4 text-xl hover:text-red-500 hover:scale-110">Henry House</h1></Link>
+      <div className="absolute right-10 space-x-10">
+        <Link to="/foro"><button className="hover:text-red-500 hover:scale-110">Foros</button></Link>
+        <Link to=""><button className="hover:text-red-500 hover:scale-110">Mensajes</button></Link>
+        <Link to="/cuenta"><button className="hover:text-red-500 hover:scale-110">Perfil</button></Link>
+        <Link to=""><button className="hover:text-red-500 hover:scale-110">Desloguea</button></Link>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar;
