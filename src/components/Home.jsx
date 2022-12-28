028d943fff58394bfa1ae92c2386a8e41619f6be
MKamui <marioelkamui@gmail.com>
2022-12-28 15:51:19 -0500

    feat(account/firebase):user data, storage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,17 +7,17 @@ function Home() {
   return (
     <>
       <Navbar />
-      <div className="flex justify-between bg-slate-100">
-        <div>
-          <div className="flex flex-col justify-center w-full h-screen ">
-            <p class="mb-4 font-extrabold tracking-tight leading-none md:text-5xl lg:text-3xl text-black">
-              USUARIOS REGISTRADOS: <span class="text-yellow-300 ">100</span>
+      <div className="flex justify-between bg-slate-200">
+        <div className="ml-[5%]">
+          <div className="flex flex-col justify-center w-full h-screen">
+            <p className="mb-4 font-extrabold tracking-tight leading-none md:text-5xl lg:text-3xl text-black">
+              USUARIOS REGISTRADOS: <span className="text-yellow-300 ">100</span>
             </p>
-            <p class="mb-4 font-extrabold tracking-tight leading-non md:text-5xl lg:text-3xl text-black">
-              POSTEOS UNICOS: <span class="text-yellow-300  ">100</span>
+            <p className="mb-4 font-extrabold tracking-tight leading-non md:text-5xl lg:text-3xl text-black">
+              POSTEOS UNICOS: <span className="text-yellow-300  ">100</span>
             </p>
-            <p class="font-extrabold tracking-tight leading-none md:text-5xl lg:text-3xl text-black">
-              ENCUENTROS EXITOSOS: <span class="text-yellow-300  ">100</span>
+            <p className="font-extrabold tracking-tight leading-none md:text-5xl lg:text-3xl text-black">
+              ENCUENTROS EXITOSOS: <span className="text-yellow-300  ">100</span>
             </p>
             <div className="flex flex-col text-center mt-10">
               <p className=" text-xl font-semibold text-gray-900">
