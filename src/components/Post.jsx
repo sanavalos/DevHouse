1334b049d986e5db7885e078bf667f635d55a598
sanavalos <santiago.avalos@outlook.com>
2023-01-04 12:09:18 -0300

    fix(Post): controlled formd

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,6 +6,7 @@ import { v4 } from "uuid";
 import { UserAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
+import Swal from "sweetalert2";
 import Footer from "./Footer";
 
 function Post() {
@@ -18,6 +19,7 @@ function Post() {
     userId: "",
     country: "",
   });
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
@@ -43,30 +45,47 @@ function Post() {
     "Venezuela",
   ];
   const { user } = UserAuth();
+
   function handleChange(e) {
     setPost({
       ...post,
       [e.target.name]: e.target.value,
     });
+    if (post.title === "") {
+      setError("El titulo es obligatorio");
+    } else if (post.country === "") {
+      setError("Selecciona un país");
+    } else if (post.comments.length < 10) {
+      setError("La descripción debe ser mayor a 10 caracteres");
+    } else {
+      setError(null);
+    }
   }
 
   const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-
-      const uuid = v4();
-      await setDoc(doc(db, "posts/" + uuid), {
-        title: post.title,
-        date: new Date().toLocaleDateString(),
-        user: user?.displayName,
-        comments: post.comments,
-        id: uuid,
-        userId: user?.uid,
-        country: post.country,
-      });
-      navigate("/foro");
-    } catch (error) {
-      console.log(error);
+    if (error === null) {
+      try {
+        e.preventDefault();
+        const uuid = v4();
+        await setDoc(doc(db, "posts/" + uuid), {
+          title: post.title,
+          date: new Date().toLocaleDateString(),
+          user: user?.displayName,
+          comments: post.comments,
+          id: uuid,
+          userId: user?.uid,
+          country: post.country,
+        });
+        Swal.fire({
+          icon: "success",
+          title: "Posteo realizado con exito",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+        navigate("/foro");
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -75,48 +94,59 @@ function Post() {
       <Navbar />
       <div className="flex justify-center h-[calc(100%_-_10.5rem)] bg-yellow-300 w-full">
         <form className="m-8">
-        <h2 className="m-8 font-extrabold text-2xl text-center">CREA TU POST</h2>
-        <div className="flex flex-col my-2">
-          <label className="py-2 font-semibold text-lg">Titulo</label>
-          <input
-            className="p-3 rounded-xl w-[800px]"
-            type="text"
-            name="title"
-            value={post.name}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-        <div className="my-6">
-          <label className="py-2 font-semibold text-lg mr-4">País donde publicar</label>
-          <select name="country" onChange={(e) => handleChange(e)} className="rounded-xl bg-black text-yellow-300">
-            {" "}
-            <option disabled selected defaultValue>
-              Selecionar un pais
-            </option>{" "}
-            {countries.map((country) => (
-              <option value={country}>{country}</option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-col my-2">
-          <label className="py-2 font-semibold text-lg">Comentario</label>
-          <textarea
-            name="comments"
-            id=""
-            cols="30"
-            rows="10"
-            value={post.comments}
-            onChange={(e) => handleChange(e)}
-            className="rounded-xl"
-          ></textarea>
-        </div>
-        <button onClick={handleSubmit} 
-        className="p-3 text-center bg-black rounded-xl text-yellow-300 my-4 hover:bg-transparent hover:border-2 hover:border-black hover:text-black w-full">
-          Enviar
-        </button>
-      </form>
-    </div>
-    <Footer />
+          <h2 className="m-8 font-extrabold text-2xl text-center">
+            CREA TU POST
+          </h2>
+          <h3 className="m-5 font-extrabold text-xl text-center">{error}</h3>
+          <div className="flex flex-col my-2">
+            <label className="py-2 font-semibold text-lg">Titulo</label>
+            <input
+              className="p-3 rounded-xl w-[800px]"
+              type="text"
+              name="title"
+              value={post.name}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div className="my-6">
+            <label className="py-2 font-semibold text-lg mr-4">
+              País donde publicar
+            </label>
+            <select
+              name="country"
+              onChange={(e) => handleChange(e)}
+              className="rounded-xl bg-black text-yellow-300"
+            >
+              {" "}
+              <option disabled selected defaultValue>
+                Selecionar un pais
+              </option>{" "}
+              {countries.map((country) => (
+                <option value={country}>{country}</option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col my-2">
+            <label className="py-2 font-semibold text-lg">Comentario</label>
+            <textarea
+              name="comments"
+              id=""
+              cols="30"
+              rows="9"
+              value={post.comments}
+              onChange={(e) => handleChange(e)}
+              className="rounded-xl p-5"
+            ></textarea>
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="p-3 text-center bg-black rounded-xl text-yellow-300 my-4 hover:bg-transparent hover:border-2 hover:border-black hover:text-black w-full"
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
+      <Footer />
     </div>
   );
 }
