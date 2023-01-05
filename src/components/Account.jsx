6948b12650ac604316dd1dbb8df243f6244cd80d
sanavalos <santiago.avalos@outlook.com>
2023-01-05 16:24:57 -0300

    fix(account): grid responsive

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import Swal from "sweetalert2";
 import Navbar from "./Navbar";
@@ -11,7 +11,8 @@ import { doc, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { getUserById } from "../redux/actions/actions";
 import { useDispatch, useSelector } from "react-redux";
-
+import { IoIosHelpCircle } from "react-icons/io";
+import { AiOutlineCloudUpload } from "react-icons/ai";
 const Account = () => {
   const { user, logout } = UserAuth();
   const dispatch = useDispatch();
@@ -57,7 +58,7 @@ const Account = () => {
     const imageURL = await getDownloadURL(imageRef);
     updateProfile(user, { photoURL: imageURL });
     let docRef = doc(db, "users", user?.uid);
-    await updateDoc(docRef, { image: imageURL } )
+    await updateDoc(docRef, { image: imageURL });
     Swal.fire({
       icon: "success",
       title: "Imagen actualizada",
@@ -108,7 +109,7 @@ const Account = () => {
     try {
       updateProfile(user, { displayName: fullname });
       let docRef = doc(db, "users", user?.uid);
-      await updateDoc(docRef, { name: fullname } )
+      await updateDoc(docRef, { name: fullname });
       Swal.fire({
         icon: "success",
         title: "Nombre actualizado",
@@ -170,7 +171,7 @@ const Account = () => {
     try {
       updateInfo({ github: github });
       let docRef = doc(db, "users", user?.uid);
-      await updateDoc(docRef, { github: github } )
+      await updateDoc(docRef, { github: github });
       Swal.fire({
         icon: "success",
         title: "Github actualizado",
@@ -228,24 +229,24 @@ const Account = () => {
     }
   };
   return (
-    <div>
+    <div className="grid h-screen">
       <Navbar />
-      <div className="flex">
-        <div className="w-[40%] bg-yellow-300 h-screen">
+      <div className="grid  grid-cols-2">
+        <div className=" bg-yellow-300">
           <div className="mt-[20%]">
             <h1 className="text-4xl text-center font-semibold">
               Nos encanta tenerte aquí
             </h1>
           </div>
-          <div>
+          <div className="mt-7 flex justify-center">
             <img
               src="https://blog.soyhenry.com/content/images/2021/02/MUNDOHENRY-1.jpg"
-              className="rounded-lg h-[520px] w-[650px] m-14"
+              className=" rounded-lg h-[520px] w-[650px] "
               alt="comunidad"
             />
           </div>
         </div>
-        <div className="w-[60%] h-screen">
+        <div className=" ">
           <div className="m-20 mt-10 border-2 border-black min-h-fit rounded-xl">
             <div className="border-b-2 border-black h-6 bg-yellow-300 rounded-t-xl"></div>
             <div className="border-b-2 border-black h-32 flex items-center space-x-8 bg-slate-300">
@@ -269,88 +270,118 @@ const Account = () => {
               <h1 className="text-2xl m-6 underline font-semibold text-center">
                 Información Personal
               </h1>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
-                  Email:
-                </label>
+              <div className="flex m-4 items-center justify-between">
+                <label className="text-xl w-20 font-semibold">Email:</label>
                 <input
-                  className="rounded-xl w-[50%] p-2 ml-20"
+                  className="rounded-xl w-[50%] p-2 "
                   placeholder={user && user.email}
                   disabled
                 ></input>
+                <Link to="/contacto">
+                  <button className="flex px-6 items-center p-1 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ">
+                    <IoIosHelpCircle size={30} />
+                  </button>
+                </Link>
               </div>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
+              <div className="flex m-4 items-center justify-between">
+                <label className="text-xl  w-20 font-semibold">
                   Contraseña:
                 </label>
                 <input
-                  className="rounded-xl w-[50%] p-2 ml-20"
+                  className="rounded-xl w-[50%] p-2 "
                   placeholder="..."
                   type={"password"}
                   onChange={handlePassword}
                 ></input>
                 <button
-                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ml-8"
+                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
                   onClick={changePassword}
                 >
                   Cambiar
                 </button>
               </div>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
-                  Nombre:
-                </label>
+              <div className="flex m-4 items-center justify-between">
+                <label className="text-xl  w-20 font-semibold">Nombre:</label>
                 <input
-                  className="rounded-xl w-[50%] p-2 ml-20"
+                  className="rounded-xl w-[50%] p-2 "
                   placeholder={user?.displayName || "Nombre Usuario"}
                   onChange={handleName}
                 ></input>
                 <button
-                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ml-8"
+                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
                   onClick={changeName}
                 >
                   Cambiar
                 </button>
               </div>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
-                  GitHub:
-                </label>
+              <div className="flex m-4 items-center justify-between">
+                <label className="text-xl  w-20 font-semibold">GitHub:</label>
                 <input
-                  className="rounded-xl w-[50%] p-2 ml-20"
+                  className="rounded-xl w-[50%] p-2 "
                   placeholder={userId?.github ?? "Link de Github"}
                   onChange={handleGithub}
                 ></input>
                 <button
-                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ml-8"
+                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
                   onClick={changeGithub}
                 >
                   Cambiar
                 </button>
               </div>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
+              <div className="flex m-4 items-center justify-between">
+                <label className="text-xl  w-20 font-semibold">
                   Descripción:
                 </label>
                 <textarea
-                  className="rounded-xl w-[50%] p-2 ml-20"
+                  className="rounded-xl w-[50%] p-2 "
                   placeholder={userId?.description ?? "Tu descripción personal"}
                   rows={"5"}
                   maxLength="250"
                   onChange={(e) => setDescription(e.target.value)}
                 ></textarea>
                 <button
-                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ml-8"
+                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
                   onClick={changeDescription}
                 >
                   Cambiar
                 </button>
               </div>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
+              <div className="flex m-4 items-center justify-between">
+                <h3 className="text-xl  w-20 font-semibold">Intereses:</h3>
+                <ul className="items-center w-[50%] text-sm font-medium rounded-lg flex bg-gray-700 border-gray-600 text-white">
+                  {intereses.map((item, index) => (
+                    <li className="w-full last:border-0 border-r border-gray-600">
+                      <div className="flex items-center pl-3">
+                        <input
+                          id={item}
+                          key={index}
+                          value={item}
+                          type="checkbox"
+                          onChange={handleCheck}
+                          className="w-4 h-4 text-blue-600 rounded ring-offset-gray-700 focus:ring-2 bg-gray-600 border-gray-500"
+                        />
+                        <label
+                          for={item}
+                          className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                          {item}
+                        </label>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+                <button
+                  className=" p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl"
+                  onClick={changeInterest}
+                >
+                  Cambiar
+                </button>
+              </div>
+              {/* <div className="flex m-4 items-center">
+                <label className="text-xl  w-20 font-semibold">
                   Intereses:
                 </label>
-                <div className="flex rounded-xl w-[50%] p-2 ml-20">
+                <div className="flex rounded-xl w-[50%] p-2 ">
                   {intereses.map((item, index) => (
                     <div key={index} className="w-full flex justify-between">
                       <div>
@@ -365,17 +396,18 @@ const Account = () => {
                   ))}
                 </div>
                 <button
-                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ml-8"
+                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
                   onClick={changeInterest}
                 >
                   Cambiar
                 </button>
-              </div>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
+              </div> */}
+              {/* ----------------------------------------- */}
+              {/* <div className="flex m-4 items-center">
+                <label className="text-xl  w-20 font-semibold">
                   Carrera:
                 </label>
-                <div className="ml-20">
+                <div className="">
                   <input
                     type="radio"
                     value="full stack"
@@ -385,7 +417,7 @@ const Account = () => {
                   />
                   Full Stack Dev
                 </div>
-                <label className="ml-20">
+                <label className="">
                   <input
                     type="radio"
                     value="data scientist"
@@ -401,29 +433,119 @@ const Account = () => {
                 >
                   Cambiar
                 </button>
-              </div>
-              <div className="flex m-4 items-center">
-                <label className="text-xl ml-32 w-20 font-semibold">
-                  Imagen:
-                </label>
-                <input
-                  className="rounded-xl w-[50%] p-2 ml-20"
-                  type={"file"}
-                  onChange={handleChange}
-                ></input>
+              </div> */}
+              <div className="flex m-4 items-center justify-between">
+                <h3 className="text-xl  w-20 font-semibold">Carrera:</h3>
+                <ul className="items-center w-[50%] text-sm font-medium rounded-lg flex bg-gray-700 border-gray-600 text-white">
+                  <li className="w-full sm:border-r border-gray-600">
+                    <div className="flex items-center pl-3">
+                      <input
+                        id="full-stack-dev"
+                        type="radio"
+                        value="full stack"
+                        checked={career === "full stack"}
+                        onChange={handleCareer}
+                        className="w-4 h-4 text-blue-600 rounded dark:focus:ring-blue-600 ring-offset-gray-700 focus:ring-2 bg-gray-600 border-gray-500"
+                      />
+                      <label
+                        for="full-stack-dev"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-300"
+                      >
+                        Full Stack Dev
+                      </label>
+                    </div>
+                  </li>
+                  <li className="w-full  dark:border-gray-600">
+                    <div className="flex items-center pl-3">
+                      <input
+                        id="data-scientist"
+                        type="radio"
+                        value="data scientist"
+                        checked={career === "data scientist"}
+                        onChange={handleCareer}
+                        className="w-4 h-4 text-blue-600 rounded dark:focus:ring-blue-600 ring-offset-gray-700 focus:ring-2 bg-gray-600 border-gray-500"
+                      />
+                      <label
+                        for="data-scientist"
+                        className="w-full py-3 ml-2 text-sm font-medium text-gray-300"
+                      >
+                        Data Scientist
+                      </label>
+                    </div>
+                  </li>
+                </ul>
                 <button
-                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ml-8"
-                  onClick={uploadImage}
+                  className=" p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl"
+                  onClick={changeCareer}
                 >
                   Cambiar
                 </button>
               </div>
+
+              {/* <div className="flex m-4 items-center justify-between">
+                <label className="text-xl  w-20 font-semibold">Imagen:</label>
+                <input
+                  className="rounded-xl w-[50%] p-2 "
+                  type={"file"}
+                  onChange={handleChange}
+                ></input>
+                <button
+                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
+                  onClick={uploadImage}
+                >
+                  Cambiar
+                </button>
+              </div> */}
+
+              <div class="flex flex-col  px-24 items-center justify-center w-full">
+                <label
+                  for="dropzone-file"
+                  class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                >
+                  <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                    <AiOutlineCloudUpload
+                      size={50}
+                      className="text-slate-200"
+                    />
+
+                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <span class="font-semibold">
+                        Click para elegir archivo
+                      </span>{" "}
+                      o arrastrar y soltar
+                    </p>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">
+                      SVG, PNG or JPG (MAX. 800x400px)
+                    </p>
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    class="hidden"
+                    onChange={handleChange}
+                  />
+                </label>
+                <div className="w-20">
+                  <div className="w-full">
+                    <div className="-mt-16">
+                      <button
+                        className="p-2 left-4 -bottom-44 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
+                        onClick={uploadImage}
+                      >
+                        Cambiar
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/*  */}
               <div className="flex m-4 items-center justify-center">
                 <button
-                  className=" p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl ml-8"
+                  className="p-2 bg-black text-yellow-300 text-md hover:scale-110 hover:text-red-500 rounded-xl "
                   onClick={getLocation}
                 >
-                  DAR LOCACION
+                  ESTABLECER UBICACIÓN
                 </button>
               </div>
             </div>
