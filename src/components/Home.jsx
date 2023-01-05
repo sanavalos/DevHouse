c477cb978335b0c93b08abb753afc7f718231635
sanavalos <santiago.avalos@outlook.com>
2023-01-04 23:36:12 -0300

    feat(home): animated count

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,21 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { getUsers, getPosts } from "../redux/actions/actions.js";
 import { useDispatch, useSelector } from "react-redux";
+import { useSpring, animated } from "react-spring";
+
+function Number({ n }) {
+  const { number } = useSpring({
+    from: { number: 0 },
+    number: n,
+    delay: 200,
+    config: { mass: 1, tension: 20, friction: 10 },
+  });
+  return (
+    <animated.div className="inline-block">
+      {number.to((n) => n.toFixed(0))}
+    </animated.div>
+  );
+}
 
 function Home() {
   const dispatch = useDispatch();
@@ -23,11 +38,15 @@ function Home() {
           <div className="flex flex-col justify-center w-full h-screen">
             <p className="mb-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl lg:text-4xl text-black">
               USUARIOS REGISTRADOS:{" "}
-              <span className="text-yellow-300 ">{users.length}</span>
+              <span className="text-yellow-300 ">
+                <Number n={users.length} />
+              </span>
             </p>
             <p className="mb-4 text-2xl font-extrabold tracking-tight leading-non md:text-3xl lg:text-4xl text-black">
               POSTEOS UNICOS:{" "}
-              <span className="text-yellow-300  ">{posts.length}</span>
+              <span className="text-yellow-300  ">
+                <Number n={posts.length} />
+              </span>
             </p>
             <div className="flex flex-col text-center mt-10">
               <p className=" text-xl font-semibold text-gray-900">
