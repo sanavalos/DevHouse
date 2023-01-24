1058e6340df0739519bd8b0bc3ba8c71ebdea478
MKamui <marioelkamui@gmail.com>
2023-01-24 16:09:58 -0500

    refactor(landing):responsive

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Landing() {
   return (
     <div className="flex flex-col md:flex-row text-center">
-      <div className="w-full md:w-[50%] bg-yellow-300 h-[50vh] md:h-screen">
+      <div className="w-full md:w-[50%] bg-yellow-300 h-1/2 md:h-screen">
         <div className="lg:m-[25%] md:mt-[60%]">
           <h1 className="text-3xl my-6 mt-12 lg:text-7xl md:text-5xl">
             ¡Bienvenido!
@@ -21,108 +21,148 @@ function Landing() {
           </Link>
         </div>
       </div>
-      <div className="lg:w-[50%] w-full md:w-[50%] bg-black h-fit md:h-screen">
-        <div className="grid grid-cols-2 gap-1 m-4 mt-14 lg:grid-cols-4 md:grid-cols-3 md:gap-2 lg:mt-36 md:mt-8">
+      <div className="w-full md:w-[50%] bg-black h-fit md:h-screen">
+        <div className="grid grid-cols-3 gap-2 m-4 mt-8 md:grid-cols-3 lg:grid-cols-4 lg:mt-36 lg:gap-4">
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1920px-Flag_of_Argentina.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Flag_of_Bolivia.svg/135px-Flag_of_Bolivia.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/749px-Flag_of_Brazil.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/800px-Flag_of_Canada.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Flag_of_Chile.svg/135px-Flag_of_Chile.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/1280px-Flag_of_Colombia.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Costa_Rica.svg/1920px-Flag_of_Costa_Rica.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Flag_of_Cuba.svg/800px-Flag_of_Cuba.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/1280px-Flag_of_Ecuador.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Flag_of_El_Salvador.svg/1920px-Flag_of_El_Salvador.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Flag_of_Guatemala.svg/1920px-Flag_of_Guatemala.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Flag_of_Honduras.svg/1920px-Flag_of_Honduras.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1920px-Flag_of_Mexico.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Nicaragua.svg/1920px-Flag_of_Nicaragua.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Panama.svg/1280px-Flag_of_Panama.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Flag_of_Paraguay.svg/1920px-Flag_of_Paraguay.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Peru.svg/1280px-Flag_of_Peru.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/800px-Flag_of_the_United_States.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/1280px-Flag_of_Uruguay.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
+          <div  className="flex justify-center">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Flag_of_Venezuela.svg/1280px-Flag_of_Venezuela.svg.png"
             className="rounded-lg h-12 w-20 md:h-[120px] md:w-[180px]"
             alt="Flag"
           />
+          </div>
         </div>
       </div>
     </div>
