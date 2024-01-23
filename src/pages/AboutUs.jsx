70aaf938c0b51969f7c3be5681654f3e015a4b6a
Santiago Avalos <89056369+sanavalos@users.noreply.github.com>
2024-01-23 19:43:23 -0300

    re-deploy commit

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -23,6 +23,34 @@ function AboutUs() {
             <div className="w-full">
               <div className="flex flex-col md:flex-row justify-evenly shadow-lg rounded-lg bg-yellow-300 m-6 ">
                 <div className="flex justify-center flex-col items-center m-4">
+                 <img
+                    className="h-64 w-60 md:h-[350px] md:w-[300px] rounded-lg border-2 border-black mt-4"
+                    src="https://avatars.githubusercontent.com/u/89056369?v=4"
+                    alt="Santi Img"
+                  />
+                  <div className="flex items-center mt-3">
+                    <p className="text-2xl leading-5 text-gray-800 font-bold">
+                      Santiago Avalos
+                    </p>
+                  </div>
+                  <div className="flex items-center space-x-6 my-4">
+                    <a
+                      href="https://github.com/sanavalos"
+                      target={"_blank"}
+                      rel="noreferrer"
+                    >
+                      <BsGithub size={30} className="hover:scale-125" />
+                    </a>
+                    <a
+                      href="https://www.linkedin.com/in/santiagoavalos/"
+                      target={"_blank"}
+                      rel="noreferrer"
+                    >
+                      <BsLinkedin size={30} className="hover:scale-125" />
+                    </a>
+                  </div>
+                </div>
+                <div className="flex justify-center flex-col items-center m-4">
                   <img
                     className="h-64 w-60 md:h-[350px] md:w-[300px] rounded-lg border-2 border-black mt-4"
                     src="https://avatars.githubusercontent.com/u/92122729?v=4"
@@ -43,34 +71,6 @@ function AboutUs() {
                     </a>
                     <a
                       href="https://www.linkedin.com/in/mario-franco-427904178/"
-                      target={"_blank"}
-                      rel="noreferrer"
-                    >
-                      <BsLinkedin size={30} className="hover:scale-125" />
-                    </a>
-                  </div>
-                </div>
-                <div className="flex justify-center flex-col items-center m-4">
-                  <img
-                    className="h-64 w-60 md:h-[350px] md:w-[300px] rounded-lg border-2 border-black mt-4"
-                    src="https://avatars.githubusercontent.com/u/89056369?v=4"
-                    alt="Santi Img"
-                  />
-                  <div className="flex items-center mt-3">
-                    <p className="text-2xl leading-5 text-gray-800 font-bold">
-                      Santiago Avalos
-                    </p>
-                  </div>
-                  <div className="flex items-center space-x-6 my-4">
-                    <a
-                      href="https://github.com/sanavalos"
-                      target={"_blank"}
-                      rel="noreferrer"
-                    >
-                      <BsGithub size={30} className="hover:scale-125" />
-                    </a>
-                    <a
-                      href="https://www.linkedin.com/in/santiagoavalos/"
                       target={"_blank"}
                       rel="noreferrer"
                     >
