72a5fd2f00a383613c7e2eb32ac2eb01cb62609c
sanavalos <santiago.avalos@outlook.com>
2025-04-03 21:02:22 -0300

    chore: new logo reference

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import Swal from "sweetalert2";
+import devhouse from "../media/devhouse.png"
 
 function Navbar() {
   const { user, logout } = UserAuth();
@@ -27,13 +28,10 @@ function Navbar() {
       <div>
         <Link to="/inicio" className="flex flex-row items-center">
           <img
-            src="https://assets.soyhenry.com/LOGO-REDES-01_og.jpg"
+            src={devhouse}
             alt="logo"
             className="md:w-14 md:h-10 w-7 h-7 border-b-2 border-black md:ml-10"
           />
-          <h1 className="md:text-2xl md:ml-2 ml-1 font-extrabold tracking-tight hover:scale-110 text-[#FFFF01]">
-            DevHouse
-          </h1>
         </Link>
       </div>
       <div className="space-x-3 mr-5 text-sm md:text-lg md:space-x-10 md:mr-10">
